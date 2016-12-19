5e225f5653202f2d331e311ae0e7366579efde68
Sam Musso <sam.musso90@gmail.com>
2016-12-19 10:54:32 -0700

    Added socketio dependency. Should send photo.jpg to server on connection

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const spawn = require('child_process').spawn;
-const ls = spawn('gphoto2', ['--trigger-capture']);
+const ss = require('socket.io-stream');
+const fs = require('fs');
+const stream = ss.createStream();
 
-ls.stdout.on('data', (data) => {
-	console.log(`stdout: ${data}`);
+const socket = require('socket.io-client')('http://10.1.10.231');
+var filename = 'photo.jpg';
+
+socket.on('connect', function(){
+
+	const gphoto = spawn('gphoto2', ['--capture-image-and-download']);
+
+	gphoto.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	gphoto.stderr.on('data', (data) => {
+	  console.log(`stderr: ${data}`);
+	});
+
+	gphoto.on('close', (code) => {
+	  console.log(`gphoto2 exited with code: ${code}`);
+		if(code === 0){
+			sendPhoto();
+		}else{
+			console.log("There was a problem capturing the photo");
+		}
+	});
 });
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
-
+var sendPhoto = function(){
+	var fileData = fs.readFileSync(`./${filename}`);
+	socket.emit('push-photo', {fd: fileData});
+};
