e6251de3c323171b244c1a6889dc6bce2b490ea7
Sam Musso <sam@alpinelaboratories.com>
2016-12-22 11:28:34 -0700

    Added some user command support

--- server.js
+++ server.js
@@ -4,14 +4,41 @@ var ss = require('socket.io-stream');
 var path = require('path');
 var fs = require('fs');
 var PORT = 80;
+var CLIENT;
+  var timeStart = Date.now();
+  var transmitTime = Date.now() - timeStart;
+
+
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+  if (key.ctrl && key.name === 'c') {
+    console.log("Goodbye");
+    process.exit();
+  }
+
+  if (key.name === 'p') {
+    timeStart = Date.now();
+    console.log("Capturing photo");
+    CLIENT.emit('photo-capture');
+  }
+
+  if (key.name === 'c') {
+    console.log("Triggering capture");
+    CLIENT.emit('trigger');
+  }
+});
 
 io.on('connection', function(socket) {
   console.log("Connection succesful!");
-  var timeStart = Date.now();
+
+  CLIENT = socket;
+
+
   socket.on('push-photo', function(data){
     console.log("got photo push");
-    console.dir(data.fd);
-    var transmitTime = Date.now() - timeStart;
+    transmitTime = Date.now() - timeStart;
     console.log("Transmit time: "+transmitTime+"ms");
 
     socket.emit('push-photo-success');
