0029fb29d2652ec65204a2fcf24a7c4fefb3fb52
Sam Musso <sam@alpinelaboratories.com>
2017-01-13 09:51:10 -0700

    Switched to libgphoto2 node wrapper from cli wrapper

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ const ss = require('socket.io-stream');
 const fs = require('fs');
 const stream = ss.createStream();
 
+const gphoto2 = require('gphoto2');
+const GPhoto = new gphoto2.GPhoto2();
+
 const CHUNK_SIZE = 102400;
+
+var camera;
+
+GPhoto.list(function (list) {
+	camera = list[0];
+});
 
 const socket = require('socket.io-client')('http://10.1.10.231:1025');
 var filename = 'photo.jpg';
@@ -11,43 +20,50 @@ var filename = 'photo.jpg';
 var start, end;
 
 function gphotoCapture(){
-	const gphoto = spawn('gphoto2', ['--capture-image-and-download', '--debug', '--filename=./'+filename]);
+	// const gphoto = spawn('gphoto2', ['--capture-image-and-download', '--debug', '--filename=./'+filename]);
+	//
+	// gphoto.stdout.on('data', (data) => {
+	//
+	// 	if(data.includes("New file is in location")){
+	// 		console.log(Date.now()+": Photo added to camera SD");
+	// 	}
+	// 	else if(data.includes("Deleting file")){
+	// 		console.log(Date.now()+": Photo added to Pulse rootfs");
+	// 	}
+	// 	else if(data.includes("Saving file as")){
+	// 		console.log(Date.now()+": Saving photo to Pulse");
+	// 	}else {
+	// 		console.log(`stdout: ${data}`);
+	// 	}
+	// });
+	//
+	// gphoto.stderr.on('data', (data) => {
+	// 	if(data.includes("gp_port_open")){
+	// 		console.log(Date.now()+": Opened ptp port");
+	// 	}
+	//
+	// 	if(data.includes("PTP_OC_NIKON_InitiateCaptureRecInMedia")){
+	// 		console.log(Date.now()+": Sending capture command")
+	// 	}
+	//   // console.log(`stderr: ${data}`);
+	// });
+	//
+	// gphoto.on('close', (code) => {
+	//   console.log(Date.now()+`: gphoto2 exited with code: ${code}`);
+	// 	if(code === 0){
+	// 		console.log("Starting photo transfer");
+	// 		sendPhoto(0);
+	// 	}else{
+	// 		console.log(Date.now()+": There was a problem capturing the photo");
+	// 	}
+	// });
 
-	gphoto.stdout.on('data', (data) => {
-
-		if(data.includes("New file is in location")){
-			console.log(Date.now()+": Photo added to camera SD");
-		}
-		else if(data.includes("Deleting file")){
-			console.log(Date.now()+": Photo added to Pulse rootfs");
-		}
-		else if(data.includes("Saving file as")){
-			console.log(Date.now()+": Saving photo to Pulse");
-		}else {
-			console.log(`stdout: ${data}`);
-		}
-	});
-
-	gphoto.stderr.on('data', (data) => {
-		if(data.includes("gp_port_open")){
-			console.log(Date.now()+": Opened ptp port");
-		}
-
-		if(data.includes("PTP_OC_NIKON_InitiateCaptureRecInMedia")){
-			console.log(Date.now()+": Sending capture command")
-		}
-	  // console.log(`stderr: ${data}`);
-	});
-
-	gphoto.on('close', (code) => {
-	  console.log(Date.now()+`: gphoto2 exited with code: ${code}`);
-		if(code === 0){
-			console.log("Starting photo transfer");
-			sendPhoto(0);
-		}else{
-			console.log(Date.now()+": There was a problem capturing the photo");
-		}
-	});
+	camera.takePicture({
+    targetPath: '/tmp/foo.XXXXXX'
+  }, function (er, tmpname) {
+    fs.renameSync(tmpname, __dirname + '/'+filename);
+		sendPhoto(0);
+  });
 }
 
 socket.on('connect', function(){
