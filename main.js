c522e57dc2bc8b4fdf9f6f98f8b1dc62afd76567
Sam Musso <sam@alpinelaboratories.com>
2017-03-22 13:04:03 -0600

    Beautify

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("* Pulse Pro Startup *");
+
 var spawn = require('child_process').spawn;
 var Q = require('q');
 var ss = require('socket.io-stream');
@@ -15,117 +17,118 @@ var net = require('net');
 var camera;
 var buffer;
 
-var tlObject = {
-  interval: 1000, // ms
-  photos: 5
-};
-
-var listCameras = function(){
-  var deferred = Q.defer();
-  console.log('Getting list of cameras...');
-  GPhoto.list(function (list) {
-  	if(list.length === 0){
-  		console.log("No camera found! Exiting...");
-  		process.exit(1);
-  	}
-  	camera = list[0];
-  	// Save pictures to sd card instead of RAM
-    deferred.resolve(camera);
-  });
-  return deferred.promise;
-};
-
-var setCameraStorage = function(cam, storage){
-  var deferred = Q.defer();
-  console.log('Setting camera storage to '+storage);
-  cam.setConfigValue('capturetarget', storage, function (er) {
-    if(er){
-      deferred.reject(er);
-    }else{
-      deferred.resolve();
-    }
-  });
-  return deferred.promise;
-};
-
-listCameras().then(function(cam){
-  setCameraStorage(cam, 1);
-});
-
 var socket = require('socket.io-client')('http://10.1.10.124:1025');
 var filename = 'photo.jpg';
 
 var start, end;
 
-function gphotoLiveView(){
-	camera.takePicture({
-	    preview: true,
-	    socket: socketPath
-	  }, function (er, tmpname) {
-				// Data is coming through IPC not callback
-	  });
-}
+var tlObject = {
+	interval: 1000, // ms
+	photos: 5
+};
 
-function gphotoCapture(dontSend){
-  camera.setConfigValue('capturetarget', 1, function(){
-    camera.takePicture({
-      download: false
-  	    // targetPath: '/foo.XXXXXX'
-  	  }, function (er, tmpname) {
-      if(er){
-        console.log("Capture error: "+er);
-      }else{
-          console.log("tmpname: "+tmpname);
-			    //  buffer = fs.readFileSync(tmpname);
-           if(!dontSend){
-			          sendPhoto(0);
-           }
-           tlObject.photos--;
-      }
-  	});
-  });
+var getCamera = function() {
+	var deferred = Q.defer();
+	console.log('Getting list of cameras...');
+	GPhoto.list(function(list) {
+		if (list.length === 0) {
+			console.log("No camera found! Exiting...");
+			process.exit(1);
+		}
+		camera = list[0];
+		// Save pictures to sd card instead of RAM
+		deferred.resolve(camera);
+	});
+	return deferred.promise;
+};
 
-}
+var setCameraStorage = function(cam, storage) {
+	var deferred = Q.defer();
+	console.log('Setting camera storage to ' + storage);
+	cam.setConfigValue('capturetarget', storage, function(er) {
+		if (er) {
+			deferred.reject(er);
+		} else {
+			deferred.resolve();
+		}
+	});
+	return deferred.promise;
+};
 
-function timelapseStep(){
-  setTimeout(function(){
-    console.log("Stepping TL... "+tlObject.photos);
-
-    gphotoCapture(true);
-    if(tlObject.photos > 1){
-      timelapseStep();
-    }
-  }, tlObject.interval);
-}
-
-socket.on('connect', function(){
-	console.log(Date.now()+": Connected to client. Awaiting commands...");
+getCamera().then(function(cam) {
+	setCameraStorage(cam, 1);
 });
 
-socket.on('capture-photo', function(){
-	console.log(Date.now()+": Initiating photo capture...");
+function gphotoLiveView() {
+	camera.takePicture({
+		preview: true,
+		socket: socketPath
+	}, function(er, tmpname) {
+		// Data is coming through IPC not callback
+	});
+}
+
+function gphotoCapture(dontSend) {
+  setCameraStorage(camera,1).then(
+    function(){
+      camera.takePicture({
+  			download: false
+  			// targetPath: '/foo.XXXXXX'
+  		}, function(er, tmpname) {
+  			if (er) {
+  				console.log("Capture error: " + er);
+  			} else {
+  				console.log("Storage Location: " + tmpname);
+  				//  buffer = fs.readFileSync(tmpname);
+  				if (!dontSend) {
+  					sendPhoto(0);
+  				}
+  				tlObject.photos--;
+  			}
+  		});
+    }
+  );
+}
+
+function timelapseStep() {
+	setTimeout(function() {
+		console.log("Stepping TL... " + tlObject.photos);
+
+		gphotoCapture(true);
+		if (tlObject.photos > 1) {
+			timelapseStep();
+		}
+	}, tlObject.interval);
+}
+
+socket.on('connect', function() {
+	console.log(Date.now() + ": Connected to client. Awaiting commands...");
+});
+
+socket.on('capture-photo', function() {
+	console.log(Date.now() + ": Initiating photo capture...");
 	gphotoCapture();
 });
 
-socket.on('live-view-frame', function(){
+socket.on('live-view-frame', function() {
 	gphotoLiveView();
 });
 
-socket.on('timelapse', function(tl){
-  if(tl && tl.interval){
-    tlObject.interval = tl.interval;
-    tlObject.photos = tl.photos;
-  }
-  console.log("Received timelapse packet!");
-  console.dir(tl);
-  timelapseStep();
+socket.on('timelapse', function(tl) {
+	if (tl && tl.interval) {
+		tlObject.interval = tl.interval;
+		tlObject.photos = tl.photos;
+	}
+	console.log("Received timelapse packet!");
+	console.dir(tl);
+	timelapseStep();
 });
 
-var sendPhoto = function(packet){
+var sendPhoto = function(packet) {
 	// console.log(Date.now()+": Pushing photo...");
 	var fileData = buffer;
 	var packets = Math.floor(fileData.length / CHUNK_SIZE);
-	if(fileData.length % CHUNK_SIZE){
+	if (fileData.length % CHUNK_SIZE) {
 		packets++;
 	}
 
@@ -133,36 +136,35 @@ var sendPhoto = function(packet){
 	socket.emit('push-photo', {
 		packet: packet,
 		packets: packets,
-		fd: fileData.slice(startIndex, startIndex + CHUNK_SIZE)});
+		fd: fileData.slice(startIndex, startIndex + CHUNK_SIZE)
+	});
 };
 
-socket.on('push-photo-success', function(data){
-	if(data.packet < data.packets - 1){
+socket.on('push-photo-success', function(data) {
+	if (data.packet < data.packets - 1) {
 		sendPhoto(data.packet + 1);
-	}else{
+	} else {
 		socket.emit('push-photo-complete');
-		console.log(Date.now()+": Photo push succesful\r\n");
-		var rm = spawn('rm', ['./'+filename]);
+		console.log(Date.now() + ": Photo push succesful\r\n");
+		var rm = spawn('rm', ['./' + filename]);
 	}
 });
 
 
 fs.stat(socketPath, function(err) {
-    if (!err) fs.unlinkSync(socketPath);
-    var unixServer = net.createServer(function(localSerialConnection) {
-        localSerialConnection.on('data', function(data) {
-						buffer = data;
-						sendPhoto(0);
-            // data is a buffer from the socket
-        });
-        // write to socket with localSerialConnection.write()
-    });
+	if (!err) fs.unlinkSync(socketPath);
+	var unixServer = net.createServer(function(localSerialConnection) {
+		localSerialConnection.on('data', function(data) {
+			buffer = data;
+			sendPhoto(0);
+		});
+	});
 
-	unixServer.listen(socketPath, function(err, path){
-		if(!err){
-		console.log("IPC Server started!");
+	unixServer.listen(socketPath, function(err, path) {
+		if (!err) {
+			console.log("IPC Server started!");
 		}
 	});
 });
 
-console.log("Initialization complete. Looking for client...");
+console.log("Init complete. Running...");
